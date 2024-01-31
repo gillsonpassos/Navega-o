9cc84a4031091cfca5e526c50c208e0b680fd8a4
pmartindev <martins20.tech@gmail.com>
2024-01-30 21:25:15 -0300

    fix: resolve error by adding react-native reanimated and gesture handler, and fix kotlin mainActivity by following the documentation from react-navigation

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+
+import {AppRegistry} from 'react-native';
 import Navegacao from './src/navegacao';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Navegacao);
--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { SafeAreaView } from "react-native";
-import { NavigationContainer } from '@react-navigation/native'
-//import Drawer from "./Drawer";
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Drawer from './Drawer';
 
-import Tab from "./Tab";
+import Tab from './Tab';
 //import Stack from "./Stack";
 
+// <Tab />
 export default props => (
-    <SafeAreaView style={{ flex: 1 }}>
-        <NavigationContainer>
-            {/* <Drawer /> */}
-            <Tab />
-            {/* <Stack /> */}
-        </NavigationContainer>
-    </SafeAreaView>
+  <SafeAreaView style={{flex: 1}}>
+    <NavigationContainer>
+      <Drawer />
+      {/* <Stack /> */}
+    </NavigationContainer>
+  </SafeAreaView>
+);
 
-)
