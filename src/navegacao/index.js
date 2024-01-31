2bdff1b177cceeb61f5eda0c52eacb93490ed128
gillsonpassos <oliveirapassos07@outlook.com>
2024-01-31 12:53:19 -0300

    Navegação Drawer #02

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import Drawer from './Drawer';
+import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import Tab from './Tab';
+import Drawer from './Drawer';
+// import Tab from './Tab';
 //import Stack from "./Stack";
 
-// <Tab />
 export default props => (
-  <SafeAreaView style={{flex: 1}}>
+  <SafeAreaView style={{ flex: 1 }}>
     <NavigationContainer>
+      {/* <Tab /> */}
       <Drawer />
       {/* <Stack /> */}
     </NavigationContainer>
